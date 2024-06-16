256638c8a9118af8bc4c06a528ef0478bab34dfb
Ivan Yakovenko <ivan2005y9@gmail.com>
2024-06-16 17:52:41 +0300

    implemented decryption

--- Assignment_3/main.cpp
+++ Assignment_3/main.cpp
@@ -13,14 +13,20 @@ char* encrypt(char* rawText, int key) {
     return rawText;
 }
 
+char* decrypt(char* encryptedText, int key) {
+    return encrypt(encryptedText, -key);
+}
+
 int main() {
-    cout << "Enter text to encrypt: ";
+//    cout << "Enter text to encrypt: ";
+    cout << "Enter text to decrypt: ";
     char *text = new char[256];
     cin.getline(text, 256);
     cout << "Enter the encryption key: ";
     int key;
     cin >> key;
-    cout << encrypt(text, key);
+//    cout << encrypt(text, key);
+    cout << decrypt(text, key);
 }
 
 
